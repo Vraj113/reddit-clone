d57bfbaefd99a5309bee339a9a714df04da3e55f
Vraj Roy <vrajroy2k11@gmail.com>
2024-10-13 23:57:24 +0530

    web Ui fixes

--- app/all/page.jsx
+++ app/all/page.jsx
@@ -8,10 +8,10 @@ const All = async () => {
   return (
     <>
       <div className="h-2"></div>
-      <div className=" rounded-3xl text-3xl font-bold   p-6 bg-white cursor-pointer md:max-w-[800px]  mx-2 my-4 ">
+      <div className=" rounded-3xl text-3xl font-bold   p-6 bg-white cursor-pointer md:max-w-[800px]  mx-2 md:mx-auto my-4 ">
         All Subreddits
       </div>
-      <div className="md:flex flex-wrap    justify-evenly  md:max-w-[800px] w-full  ">
+      <div className="md:flex flex-wrap    justify-evenly  md:max-w-[800px]   mx-2 md:mx-auto ">
         {allSubs.map((subreddit) => {
           return (
             <Link key={subreddit.name} href={`r/${subreddit.name}`}>
